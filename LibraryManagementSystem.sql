f03ab3d0192cd67704d6b154cca134b013475e54
Maddy Haldeman <madelyn.haldeman@gmail.com>
2019-03-05 17:15:38 -0800

    insert data into first two tables; bug encountered in line 69: The INSERT statement conflicted with the FOREIGN KEY constraint 'fk_PublisherName'.

--- LibraryManagementSystem.sql
+++ LibraryManagementSystem.sql
@@ -8,7 +8,7 @@ create table LIBRARY_BRANCH (
 );
 
 create table PUBLISHER (
-	PublisherName varchar(50) primary key not null,
+	PublisherName varchar(100) primary key not null,
 	Address varchar(100) not null,
 	Phone varchar(30) not null
 );
@@ -16,8 +16,9 @@ create table PUBLISHER (
 create table BOOKS (
 	BookID int primary key not null identity (200, 1),
 	Title varchar(50) not null unique,
-	PublisherName varchar(50) not null constraint fk_PublisherName foreign key references PUBLISHER(PublisherName) on update cascade on delete cascade
+	Publisher_Name varchar(100) not null constraint fk_PublisherName foreign key references PUBLISHER(PublisherName) on update cascade on delete cascade
 );
+
 
 create table BOOK_AUTHORS (
 	BookID int not null constraint fk_BookID foreign key references BOOKS(BookID) on update cascade on delete cascade,
@@ -45,3 +46,51 @@ create table BOOK_LOANS	(
 	DateDue date
 );
 
+/*Populate Tables*/
+insert into LIBRARY_BRANCH
+	(BranchName, Address)
+	values
+	('Central', '172 1st Street'),
+	('Sharpstown', '819 16th Avenue'),
+	('SODO', '1739 3rd Ave N'),
+	('Eastgate', '119 Main Street')
+;
+
+insert into PUBLISHER
+	(PublisherName, Address, Phone)
+	values
+	('Hachette Livre', '221B Bakers Street', '615-374-1937'),
+	('Bloomsbury Publishing', '891 Wallingford Ave', '206-186-2836'),
+	('Macmillian Publishers', '718 121st Lane', '253-137-1937'),
+	('Simon & Schuster', '2837 N 156th Place', '775-863-3862'),
+	('George Allen & Unwin', '1763 8th Street', '318-382-4842')
+;
+
+insert into BOOKS
+	(Title, Publisher_Name)
+	values
+	('The Lost Tribe', 'Macmillian Publishers'),
+	('IT', 'Simon & Schuster'),
+	('The Green Mile', 'Simon & Schuster'),/*Steven King*/ 
+	('Pet Sematary', 'Simon & Schuster'),
+	('The Shining', 'Simon & Schuster'),
+	('Carrie', 'Simon & Schuster'),
+	('Julies Wolf Pack', 'HarperCollins'), /*Jean Craighead George*/
+	('An Assassins Guide to Love and Treason', 'Hachette Livre'), /*Virginia Boecker*/
+	('Muse of Nightmares', 'Hachette Livre'),/*Laini Taylor*/
+	('Nightblood', 'Hachette Livre'),/*Elly Blake*/
+	('The Big Nine', 'Hachette Livre'),/*Amy Webb*/
+	('The Hobbit', 'George Allen & Unwin'),/*J.R.R. Tolkien*/
+	('The Lord of the Rings', 'George Allen & Unwin'),
+	('White Fang', 'Macmillian Publishers'),/*Jack London*/
+	('The Call of the Wild', 'Macmillian Publishers'),
+	('Harry Potter and the Socerers Stone', 'Bloomsbury Publishing'),/*JK Rowling*/
+	('Harry Potter and the Chamber of Secrets', 'Bloomsbury Publishing'),
+	('Harry Potter and the Prisoner of Azkaban', 'Bloomsbury Publishing'),
+	('Harry Potter and the Goblet of Fire', 'Bloomsbury Publishing'),
+	('Harry Potter and the Order of the Pheonix', 'Bloomsbury Publishing'),
+	('Harry Potter and the Half Blood Prince', 'Bloomsbury Publishing'),
+	('Harry Potter and the Deathly Hallows', 'Bloomsbury Publishing')
+;
+select * from PUBLISHER;
+select * from BOOKS;

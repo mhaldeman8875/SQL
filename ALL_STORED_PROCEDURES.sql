cdd09d5045b7c2b44799e177d4b006d92e767170
Maddy Haldeman <madelyn.haldeman@gmail.com>
2019-03-06 20:30:13 -0800

    debug final stored procedure

---
+++ ALL_STORED_PROCEDURES.sql
@@ -0,0 +1,18 @@
+use LibraryManagementSystem
+
+exec [dbo].[populate_LibraryManagementSystem]
+
+exec [dbo].[TotalLoans_byBRANCH]
+
+exec [dbo].[BORROWERS_inactive]
+
+exec [dbo].[MoreThan5_info_BORROWER]
+
+exec [dbo].[Sharpstown_Due_Today_BORROWER]
+
+exec [dbo].[StephenKing]
+
+exec [dbo].[TheLostTribe_Copies_ByBRANCH]
+
+exec [dbo].[TheLostTribe_Sharpstown_Copies]
+
--- dbo_MoreThan5_info_BORROWER.sql
+++ dbo_MoreThan5_info_BORROWER.sql
@@ -3,20 +3,8 @@ use LibraryManagementSystem
 go
 create proc dbo.MoreThan5_info_BORROWER
 as
-
-go
-	alter table BOOK_LOANS
-		add Name varchar(50) null,
-		Address varchar(50) null;
-go
-	update BOOK_LOANS
-	set Name = a.Name from BOOK_LOANS b INNER JOIN BORROWER a on a.CardNo = b.CardNo;
-go 
-	update BOOK_LOANS
-	set Address = a.Address from BOOK_LOANS b INNER JOIN BORROWER a on a.CardNo = b.CardNo;
-go
-	select Name, Address, count(BookID) 'Total Loans'
-	from BOOK_LOANS 
-	group by Name, Address
-	having count(BookID) > 5;
+	select a.Name, a.Address, COUNT(b.BookID) as 'Active Loans'
+	from (BORROWER a
+	inner join BOOK_LOANS b on b.CardNo = a.CardNo)
+	Group by a.Name, a.Address
 go
